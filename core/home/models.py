136093d85b82a3657ad7750773b013228d58989e
PratyushPoddar07 <shuvampoddar018@gmail.com>
2025-01-04 21:15:53 +0530

    shell

--- core/home/models.py
+++ core/home/models.py
@@ -6,6 +6,5 @@ class Student(models.Model):
     age = models.IntegerField()
     email = models.EmailField()
     address = models.TextField(null=True,blank=True)
-    image =models.ImageField()
-    file = models.FileField()
+    
      
---
+++ core/home/utils.py
@@ -0,0 +1,6 @@
+from .models import Student
+import time
+def run_this_function():
+    print("Function started")
+    time.sleep(5)
+    print("Function Executed")
