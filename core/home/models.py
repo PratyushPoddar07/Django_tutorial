a3768a6a2f84e4192a61ad262c86d9053b6b3ada
PratyushPoddar07 <shuvampoddar018@gmail.com>
2025-01-04 22:39:38 +0530

    CRUD-OPERATION

--- core/home/models.py
+++ core/home/models.py
@@ -7,4 +7,10 @@ class Student(models.Model):
     email = models.EmailField()
     address = models.TextField(null=True,blank=True)
     
-     
+    
+class Car(models.Model):
+    car_name =models.CharField(max_length=500)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return self.car_name
--- core/home/utils.py
+++ core/home/utils.py
@@ -2,5 +2,5 @@ from .models import Student
 import time
 def run_this_function():
     print("Function started")
-    time.sleep(5)
+    time.sleep(2)
     print("Function Executed")
