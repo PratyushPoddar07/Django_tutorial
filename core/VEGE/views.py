d9e9ff7290f983bc0928d2dae965037f77b4e55c
PratyushPoddar07 <shuvampoddar018@gmail.com>
2025-01-07 14:22:31 +0530

    check

--- core/VEGE/views.py
+++ core/VEGE/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def recipe(request):
+    if request.method == "POST":
+        data = request.POST
+
+        recipe_image = request.FILES.get('recipe_image')
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+        
+        print(recipe_description) 
+        print(recipe_name) 
+        print(recipe_image) 
+
     return render(request,'recipe.html')
